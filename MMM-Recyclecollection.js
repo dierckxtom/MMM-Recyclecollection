fa79d9afa0d21f5e0cf5c50cdfed9c81040bec0c
dierckxtom <155065601+dierckxtom@users.noreply.github.com>
2024-11-19 09:47:46 +0100

    Update MMM-Recyclecollection.js

--- MMM-Recyclecollection.js
+++ MMM-Recyclecollection.js
@@ -49,7 +49,7 @@ socketNotificationReceived: function (notification, payload) {
         var collectionItem = document.createElement("div");
         collectionItem.classList.add("collection-item");
         collectionItem.innerHTML = `
-          <div><strong>Afval: ${item.fractionName}</strong></div>
+          <div><strong>Fractie: ${item.fractionName}</strong></div>
           <div>Ophaaldatum: ${item.timestamp}</div>
         `;
         wrapper.appendChild(collectionItem);
